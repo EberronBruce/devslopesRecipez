4c2aa523b1eb2fdd00c07f2db9659730b63d6eb0
Bruce Burgess <bruceburgess67@yahoo.com>
2016-04-02 17:23:31 +0700

    Added some classes and adjust some things for the tableview

---
+++ recipez/RecipeCell.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeCell.swift
+//  recipez
+//
+//  Created by Bruce Burgess on 4/2/16.
+//  Copyright © 2016 Red Raven Computing Studios. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+    
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(recipe: Recipe) {
+        recipeTitle.text = recipe.title
+        recipeImg.image = recipe.getRecipeImg()
+    }
+
+}
